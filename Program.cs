bf789f1c3e0ef15db17335b8e176c99493f5e3dc
chime08 <clhamo558@gmail.com>
2026-03-10 09:09:28 -0400

    test Gem class

--- Program.cs
+++ Program.cs
@@ -7,5 +7,25 @@ namespace Bejeweled.Core;
 
 class Program
 {
-    
+    static void Main(string[] args)
+    {
+        Gem gem1 = new Gem(GemType.Red);
+        Gem gem2 = new Gem(GemType.Blue);
+        Gem gem3 = new Gem(GemType.Red);
+        Gem gem4 = new Gem(GemType.Green);
+        Gem gem5 = new Gem(GemType.Yellow);
+        Gem gem6 = new Gem(GemType.Purple);
+
+        Console.WriteLine("\n-----------------------------\n");
+        Console.WriteLine("Welcome to the Bejewelwd Game");
+        Console.WriteLine("\n-----------------------------\n");
+        Console.WriteLine("List of Gems\n");
+
+        Console.WriteLine(gem1);
+        Console.WriteLine(gem2);
+        Console.WriteLine(gem3);
+        Console.WriteLine(gem4);
+        Console.WriteLine(gem5);
+        Console.WriteLine(gem6);
+    }
 }
--- obj/Debug/net10.0/Bejeweled.AssemblyInfo.cs
+++ obj/Debug/net10.0/Bejeweled.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bejeweled")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ba58bf8e31bc1aa899a98daf0d25bd0b853a52f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de02e896e0824d8d5f2312a14777cf0ae74f1c2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bejeweled")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bejeweled")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
